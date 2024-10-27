242a95e88ee17a885fc3fd63678cf02704ef4828
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-10-27 23:03:39 +0700

    feat: setup next-theme

---
+++ components/providers/root-provider.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export default function RootProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
