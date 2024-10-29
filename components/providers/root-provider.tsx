21602d8790b6a180d0b5d24ec583070b0ca64687
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-10-29 21:07:18 +0700

    setup simple tanstack query + devtools

--- components/providers/root-provider.tsx
+++ components/providers/root-provider.tsx
@@ -1,19 +1,27 @@
+"use client";
+
 import { ThemeProvider } from "next-themes";
-import React from "react";
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function RootProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
